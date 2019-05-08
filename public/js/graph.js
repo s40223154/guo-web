5171a60cb805c34d7b2c602612db820d165da2fe
s40223154 <40223154@gm.nfu.edu.tw>
2019-05-09 01:10:28 +0800

    20190506
    addY

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,5 +1,5 @@
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
 /**
@@ -10,33 +10,79 @@ function getRandomArbitrary(min, max) {
  * Using Math.round() will give you a non-uniform distribution!
  */
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 var chart = c3.generate({
-    data: {
-        columns: [
-            ['VCO2', 0.03, 3.2, 1.8, 4.2, 1.9, 4.8, 1.95, 4.1, 2, 5, 1.9, 5.1, 2.4],
-            ['VO2', 20, 17, 21.2, 16.8, 20.8, 16.5, 19.8, 17, 21.2, 16.8, 20.8, 16.5, 19.8],
-            ['VE', 9, 10, 10.5, 18.5, 11.6, 10.1, 9.89, 12.5, 10.5, 16.5, 13.5, 10.5, 8.5]
-        ],
-        type: 'area-spline',
-        colors: {
-            VCO2: '#00f000',
-            VO2: '#ff0000',
-            VE: '#0000ff'
-        },
-        labels: true
+  data: {
+    x: 'x',
+    columns: [
+      ['x', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13 ,14,15,16,17,18,19],
+      ['VCO2', 0,0,0,0,0,0,0,0,0,0,0,0],
+      ['VO2'],
+      ['VE']
+    ],
+    axes: {
+      data1: 'VO2',
+      data2: 'VCO2'
+    },
+    colors: {
+      VCO2: '#10e610',
+      VO2: '#ff0000',
+      VE: '#0000ff'
+    },
+    type: 'spline',
+    labels: true
+  },
+  axis: {
+    x: {
+      type: 'category',
+      categories: ['adsa', 'das']
+    },
+    y: {
+      tick: {
+        format: function(x) {
+          return x % 1 === 0 ? x : '';
+        }
+      }
+    },
+    y2: {
+      show: true
     }
+  }
 });
 
+var i = -1;
+var org_time = ['x', 0,1];
+var co2 = ['VCO2', getRandomArbitrary(0.03, 6)];
+var o2 = ['VO2', getRandomInt(16, 21), getRandomInt(19, 20), getRandomInt(19, 21), getRandomInt(19, 20), getRandomInt(18, 21), getRandomInt(19, 22), getRandomInt(19, 21), getRandomInt(19, 20), getRandomInt(19, 21), getRandomInt(19, 20), getRandomInt(18, 21), getRandomInt(19, 22)];
+var ve = ['VE', getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15)];
+
 setInterval(function() {
-    chart.load({
-        columns: [
-            ['VCO2', getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6), getRandomInt(0.03, 6)],
-            ['VO2', getRandomInt(16, 21), getRandomInt(19, 20), getRandomInt(19, 21), getRandomInt(19, 20), getRandomInt(18,21), getRandomInt(19, 22), getRandomInt(19, 21), getRandomInt(19, 20), getRandomInt(19, 21), getRandomInt(19, 20), getRandomInt(18,21), getRandomInt(19, 22)],
-            ['VE', getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5,15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5, 15), getRandomInt(5,15), getRandomInt(5, 15)]
-        ]
-    });
+  chart.load({
+    columns: [
+      org_time,
+      co2,
+      o2,
+      ve
+    ],
+    axis: {
+      x: {
+        categories: ['adsa', 'das', 'adsa', 'das', 'adsa', 'das', 'adsa', 'das']
+      }
+    }
+  })
+  if (co2.length > 32) {
+    co2.splice(1, 1);
+  }
+  co2.push(getRandomInt(0.03, 6));
+  if (org_time.length > 32) {
+    org_time.splice(1, 1);
+
+    i++;
+  }
+
+  org_time.push(i);
+  console.log(i);
 }, 1000);
