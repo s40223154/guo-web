3dc56714fd70b1f2e9f4872efe8e000070f6c503
s40223154 <40223154@gm.nfu.edu.tw>
2019-08-19 00:13:56 +0800

    20190819 dayi update
    good

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,89 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('hihihi');
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/getData', function(req, res, next) {
+  console.log(req.query.red);
+  var MongoClient = require('mongodb').MongoClient;
+  let data = {
+    red: req.query.red,
+    green: req.query.green,
+    blue: req.query.blue,
+    flow: req.query.Flow,
+    time: req.query.T
+  };
+
+  MongoClient.connect('mongodb://localhost:27017/sensor', function(err, db) {
+    if (err) {
+      console.log("Please check you db connection parameters");
+    } else {
+      db.collection('Friend', function(err, collection) {
+        if (err) {
+          db.close();
+          console.log(err);
+        }
+        collection.insert(data, {
+          safe: true
+        }, function(err, ebook) {
+          db.close();
+          if (err) {
+            console.log(err);
+          }
+          console.log("Saved !");
+        });
+      });
+    }
+  });
+
+});
+
+router.get('/showToWeb', function(req, res, next) {
+  var MongoClient = require('mongodb').MongoClient;
+  let data = req.body;
+  console.log(req.query.data);
+
+  MongoClient.connect('mongodb://localhost:27017/sensor', function(err, db) {
+    if (err) {
+      console.log("Please check you db connection parameters");
+    } else {
+      db.collection('Friend', function(err, collection) {
+        if (err) {
+          db.close();
+          console.log(err);
+        }
+        collection.find({}, {
+          safe: true
+        }, function(err, datas) {
+          console.log(datas);
+          db.close();
+          if (err) {
+            console.log(err);
+          }
+          console.log("Saved !");
+          res.end();
+        });
+      });
+    }
+  });
+
 });
 
 router.get('/new', function(req, res, next) {
-  res.render('new', { title: 'Express' });
+  res.render('new', {
+    title: 'Express'
+  });
 });
 
 router.post('/getData', function(req, res, next) {
   console.log(req.body.passwd);
-  res.render('new', { title: 'Express' });
+  res.render('new', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
